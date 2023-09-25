971980bad95e2c31868941fbb279048a5500ceb7
zeio <zeionara@gmail.com>
2023-09-25 22:41:49 +0300

    feat(hugging-face): added a couple of experiments with models from hugging face

--- less/__main__.py
+++ less/__main__.py
@@ -1,5 +1,7 @@
 from click import group, argument
 
+from langchain import PromptTemplate, HuggingFaceHub, LLMChain
+
 
 @group()
 def main():
@@ -9,7 +11,38 @@ def main():
 @main.command()
 @argument('text', type = str)
 def act(text: str):
-    print(text)
+    template = PromptTemplate(
+        template = 'question: {question}? answer:',
+        input_variables = ('question', )
+    )
+
+    # prompt = template.format(question = text)
+
+    # print(prompt)
+
+    hub = HuggingFaceHub(
+        repo_id = 'google/flan-t5-large',
+        model_kwargs = {
+            'temperature': 1e-10
+        }
+    )
+
+    chain = LLMChain(
+        prompt = template,
+        llm = hub
+    )
+
+    print('evaluating...')
+
+    print(chain.run(text))
+    # print(chain.generate(
+    #     [
+    #         {
+    #             'question': question
+    #         } for question in text.split(',')
+    #     ]
+    # ))
+    # print(chain.run(text.split(',')))
 
 
 if __name__ == '__main__':
